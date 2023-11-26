06bf501214329e1a26de24aaadc50cf57b27eae9
rrguntaka <raj@guntaka.com>
2023-11-26 23:47:19 +0000

    add seating configuration and seat allocations

--- python_work/airtravel.py
+++ python_work/airtravel.py
@@ -12,6 +12,8 @@ class Flight:
         
         self._number = number
         self._aircraft = aircraft
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
     
     def aircraft_model(self):
         return self._aircraft._model
@@ -21,6 +23,79 @@ class Flight:
     
     def airline(self):
         return number[:2]
+    
+    def allocate_seat(self, seat, passenger):
+        """Allocate a seat to a passenger.
+
+        Args:
+            seat: A seat designator such as '12C' or '11F'
+            passenger: The passenger name.
+        
+        Raises:
+            ValueError: If the seat is unavailable.
+        """
+        row, letter = self._parse_seat(seat)
+        
+        if self._seating[row][letter] is not None:
+            raise ValueError(f"Seat {seat} already occupied")
+        
+        self._seating[row][letter] = passenger
+
+    def _parse_seat(self, seat):
+        rows, seat_letters = self._aircraft.seating_plan()
+
+        letter = seat[-1]
+        if letter not in seat_letters:
+            raise ValueError(f"Invalid seat row {letter}")
+        
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError(f"Invalid seat row {row_text}")
+        
+        if row not in rows:
+            raise ValueError(f"INvalid row number {row}")
+        
+        return row, letter
+    
+    def relocate_passenger(self, from_seat, to_seat):
+        """Relocate a passenger to a differnet seat.
+
+        Args:
+            from_seat: The existing seat designated for the
+                       passenger to be moved.
+            
+            to_seat: The new seat designator.
+        """
+        from_row, from_letter = self._parse_seat(from_seat)
+        if self._seating[from_row][from_letter] is None:
+            raise ValueError(f"No passenger to relocate in seat {from_seat}")
+        
+        to_row, to_letter = self._parse_seat(to_seat)
+        if self._seating[to_row][to_letter] is not None:
+            raise ValueError(f"Seat {to_seat} already occupied")
+        
+        self._seating[to_row][to_letter] = self._seating[from_row][from_letter]
+        self._seating[from_row][from_letter] = None
+    
+    def num_available_seats(self):
+        return sum(sum(1 for s in row.values() if s is None) for row in self._seating[1:])
+    
+    def make_boaring_cards(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger, seat, self._number, self.aircraft_model())
+    
+    def _passenger_seats(self):
+        """An iterable series of passenger seating locations."""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, f"{row}{letter}")
+    
+    
 
 
 class Aircraft:
@@ -39,7 +114,27 @@ class Aircraft:
     
     def seating_plan(self):
         return (range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row])
-    
 
-    
-    
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = f"| Name: {passenger}"          \
+             f"  Flight: {flight_number}"    \
+             f"  Seat: {seat}"    \
+             f"  Aircraft: {aircraft}"    \
+             " |"
+    banner = "+" + "-" * (len(output) - 2) + "+"
+    border = "|" + " " * (len(output) - 2) + "|"
+    lines = [banner, border, output, border, banner]
+    card = "\n".join(lines)
+    print(card)
+    print()
+
+
+def make_flight():
+    f = Flight("BA758", Aircraft("G-EUPT", "Airbus A319", num_rows=22, num_seats_per_row=6))
+    f.allocate_seat("12A", "Guido")
+    f.allocate_seat("15F", "Bjarne")
+    f.allocate_seat("15E", "Anders")
+    f.allocate_seat("1C", "John")
+    f.allocate_seat("1D", "Rich")
+    f.allocate_seat("2A", "Raj")
+    return f
